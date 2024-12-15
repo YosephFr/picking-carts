b695ffca8ee462f76336b08d2c2c9e110996b543
Yoseph Franco <yosephfr@icloud.com>
2024-12-15 12:29:14 -0400

    mejora en la fecha

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,22 @@
-export function setCurrentDate(elementId){
-    const dateElement=document.getElementById(elementId);
-    if(dateElement){
-        const today=new Date().toLocaleDateString();
-        if(dateElement.tagName==='INPUT'){
-            dateElement.value=today;
-        }else{
-            dateElement.textContent=today;
+export function setCurrentDate(elementId) {
+    const dateElement = document.getElementById(elementId);
+    if (dateElement) {
+        const now = new Date();
+        const options = { 
+            year: 'numeric', 
+            month: 'long', 
+            day: 'numeric', 
+            hour: 'numeric', 
+            minute: 'numeric', 
+            second: 'numeric', 
+            hour12: true 
+        };
+        const formattedDate = now.toLocaleString('en-US', options);
+
+        if (dateElement.tagName === 'INPUT') {
+            dateElement.value = formattedDate;
+        } else {
+            dateElement.textContent = formattedDate;
         }
     }
 }
