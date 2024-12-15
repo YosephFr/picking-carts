bb619b6b75acc0efb37fec77ea910510cfed8d9f
Yoseph Franco <yosephfr@icloud.com>
2024-12-15 12:22:58 -0400

    agregado config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// config.js
+export const MAX_BINS = 5;
+
+export const MESSAGES = {
+    maxBinsReached: 'You can only add up to 5 bins.',
+    fillNextCart: 'Please fill in the details for the next picking cart.',
+    formSuccess: 'Form submitted successfully!',
+    validationError: 'Please ensure all required fields are correctly filled out.'
+};
